0c9aef425d92b52b359db042ecbfb0ef46fe8a91
Junior-Liberato <adilson.liberato.tradimaq@gmail.com>
2026-07-03 09:55:57 -0300

    Update Firebase configuration with new credentials

--- js/firebase.js
+++ js/firebase.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyAIlBiewiurKDN0_hfCRDu1Wjyxt3595hc',
+  authDomain: 'sistema-financeiro-famil-446f8.firebaseapp.com',
+  projectId: 'sistema-financeiro-famil-446f8',
+  storageBucket: 'sistema-financeiro-famil-446f8.firebasestorage.app',
+  messagingSenderId: '125699463144',
+  appId: '1:125699463144:web:2972be3fd2c416a572b65d',
+  measurementId: 'G-7QREDELHPN'
 };
