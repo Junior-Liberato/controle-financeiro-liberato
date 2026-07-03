5e7cb2c055d1373a75dc716913d0851fdccb5301
Junior-Liberato <adilson.liberato.tradimaq@gmail.com>
2026-07-03 10:55:22 -0300

    Criar inicialização Firebase

---
+++ js/firebase-init.js
@@ -0,0 +1,8 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js';
+import { firebaseConfig } from './firebase.js';
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
